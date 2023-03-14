f1969262cf30319bfb9d85bdcdfe28a3f822473f
usernameJeremy <Jeremy_koster@outlook.com>
2023-03-14 09:51:50 +0100

    form toegevoegdd bij cart component 	modified:   src/app/cart/cart.component.html 	modified:   src/app/cart/cart.component.ts

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,18 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  templateUrl: '/cart.component.html',
+  styleUrls: ['/cart.component.css']
 })
 export class CartComponent {
 
   items = this.cartService.getItems();
+  
+  //Het format van het form 
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: ''
+  });
+
 
   constructor(
-    private cartService: CartService
+    private cartService: CartService,
+    private formBuilder:  FormBuilder,
   )  {}
+
+  //het submitten van het formulier
+  onSubmit(): void{
+    //process checkout data here
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    //terug leeg maken van form
+    this.checkoutForm.reset();
+  }
+
 
 
 }
